837688988739414c6cc22f0bae7fe61a93230fd1
Priyammaurya1 <priyammaurya643@gmail.com>
2025-02-27 19:32:02 +0530

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 354 - 46;
+  final String name = "Cooler";
+  HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFF9000),
+        title: Text('Catalog App'),
+      ),
+      body: Center(child: Container(child: Text('welcome home $days $name'))),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
+import 'package:coffee_card/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('welcome home'),
-         ),
-       ),
-      )
-    );
+    // double pi = 3.14;
+    // bool isMale = true;
+
+    // var day = "Saturday";
+    // const pie = 3.14;
+
+    return MaterialApp(home: HomePage());
   }
 }
