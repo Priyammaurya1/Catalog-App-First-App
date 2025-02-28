b2c92b64ce89c70c4e058168c49fb2c0db3c25cf
Priyammaurya1 <priyammaurya643@gmail.com>
2025-02-28 15:00:42 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_card/pages/home_page.dart';
 import 'package:coffee_card/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        primaryTextTheme: GoogleFonts.lateefTextTheme(),
+        ),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      // initialRoute: "/home",
       routes: {
         "/": (context) => LogInpage(),
         "/home": (context) => HomePage(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -4,15 +4,18 @@ class LogInpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Login Page",
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assets/login.png",
+          fit: BoxFit.cover,
+          ),
+        Text("Welcome",
         style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
+          fontSize: 22,
           fontWeight: FontWeight.bold,
-        ),
-        )
-      )
-    );
+        ),)
+        ],
+    ));
   }
 }
