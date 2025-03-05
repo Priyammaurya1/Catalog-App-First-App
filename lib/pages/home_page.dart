bbe400c678119e387ed0f8581d72fbe04ed4e37f
Priyammaurya1 <priyammaurya643@gmail.com>
2025-03-05 17:49:20 +0530

    added_json_file_as_well_as_mappin_some

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,41 @@
+import 'dart:convert';
 import 'package:coffee_card/models/catalog.dart';
 import 'package:flutter/material.dart'; 
 import 'package:coffee_card/widgets/drawer.dart';
 import 'package:coffee_card/widgets/item_widget.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 354 - 46;
-  final String name = "Cooler";
+
+
+class HomePage extends StatefulWidget {
+
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 354 - 46;
+
+  final String name = "Cooler";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async{
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    final productsData = decodedData["products"];
+    print(productsData); 
+  }
+
+  // void print(String catalogJson){
+  //   print(catalogJson);
+  // }
 
   @override
   Widget build(BuildContext context) {
